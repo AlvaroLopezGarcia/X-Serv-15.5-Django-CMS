943f5ad6a492f096818d4d2399c28271135c0e8d
Alvaro Lopez Garcia <lopeza@alpha25.aulas.gsyc.urjc.es>
2018-04-04 17:59:53 +0200

    Version definitiva

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+
+from .models import Pages
+
+def barra(request):
+    lista = Pages.objects.all()
+    respuesta = "<ul>"
+    for pagina in lista:
+        respuesta += '<li><a href= "/pages/' + str(pagina.id) + '">' + pagina.name + "</a>"
+    respuesta += "</ul>"
+    return HttpResponse (respuesta)
+
+@csrf_exempt
+def pages (request, numero):
+    if request.method == "POST":
+        print (request.POST['name'], request.POST['page'])
+        page = Pages(name = request.POST['name'], page = request.POST['page'])
+        page.save()
+
+    try:
+        page = Pages.objects.get(id=str(numero))
+    except Pages.DoesNotExist:
+        return HttpResponseNotFound('<h1>' + numero + ' not found</h1>')
+    return HttpResponse(page.name + " " + str(page.page))
--- myproject/urls.py
+++ myproject/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'myproject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'cms.views.barra'),
+    url(r'^pages/(\d+)$','cms.views.pages'),
     url(r'^admin/', include(admin.site.urls)),
 )
